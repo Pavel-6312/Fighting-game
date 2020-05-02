9fde4148744c77f831125bf52068dd347706b480
P <morr.conceptart@gmail.com>
2020-05-02 12:51:23 +0200

    1

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ var config = {
         type: Phaser.AUTO,
         width: 400,
         height: 300,
+        parent: 'game1', //id that can be added to html tag to insert the game there
         backgroundColor: 0xffffff,
-        pixelArt: true,
-        zoom: 1.5,
+        pixelArt: true, //fix blurred pixels
+        zoom: 1.5, //zoom th egame
         physics: {
             default: "arcade",
             arcade: {
-                gravity: {y: 500},
+                gravity: {y: 1200},
                 debug: false
             }
         },
@@ -20,11 +21,10 @@ var config = {
         }
     };
 
-// Variables
+// Variables (declare all variable that are used below)
 var game = new Phaser.Game(config);
-
-
 var player;
+var char2;
 var cursors;
 var platforms;
 
@@ -36,22 +36,47 @@ function preload (){
     this.load.spritesheet('p1-walk', 'assets/character/2 GraveRobber/GraveRobber_run.png', {frameWidth: 48, frameHeight: 48,})
     this.load.spritesheet('p1-jump', 'assets/character/2 GraveRobber/GraveRobber_jump.png', {frameWidth: 48, frameHeight: 48,})
 
+    this.load.spritesheet('p2-walk', 'assets/desert-enemy/6 Deceased/Deceased_walk.png', {frameWidth: 48,frameHeight: 48,});
+    this.load.spritesheet('p2-death', 'assets/desert-enemy/6 Deceased/Deceased_death.png', {frameWidth: 48,frameHeight: 48,});
+
 }
 
 function create (){
 //Environment
-    platforms = this.physics.add.staticGroup();
-    platforms.create(300, 284, 'ground');
+    platforms = this.physics.add.staticGroup(); //Adds ground 
+    platforms.create(300, 284, 'ground'); //Places ground sprite
 
-// Player
-    player = this.physics.add.sprite(200, 40, 'p1-idle');
-    player.setCollideWorldBounds(true);
-    player.body.setGravityY(1200);
+//Enemy
+    char2 = this.add.sprite(0,0, 'p2-walk')
+    char2.setOrigin(0.5,0.5); //X Y Sets center point to the top left
+    char2.y = game.config.height/2+70; //Center position
+    char2.x = game.config.width/2+40;
+    char2.alpha=1; //Transparency
+    char2.angle=0; //Rotation
+    char2.scaleX=2; //Scale
+    char2.scaleY=1; 
+    char2.scaleY = char2.scaleX; //Syncs scale values
+    // char2.displayWidth=100; //Size in pixels
+    // char2.displayHeight=100;
+
+    this.anims.create({
+        key: 'p2-walk',
+        frames: this.anims.generateFrameNumbers('p2-walk', { start: 0, end: 6 }),
+        frameRate: 6,
+        repeat: -1 // -1 run forever / 1 -> run once
+    });
+
+    char2.anims.play('p2-walk',true);
+
+//Player
+    player = this.physics.add.sprite(game.config.width/2-40, game.config.height/2, 'p1-idle'); //Places sprite in the center of the screen
+    player.setCollideWorldBounds(true); //Collides with world bounds
+    // player.body.setGravityY(1200); //Override gravity
     
     this.anims.create({
-            key: 'p1-idle',
-            frames: this.anims.generateFrameNumbers('p1-idle', { start: 0, end: 4 }),
-            frameRate: 6,
+        key: 'p1-idle',
+        frames: this.anims.generateFrameNumbers('p1-idle', { start: 0, end: 4 }),
+        frameRate: 6,
         });
 
     this.anims.create({
@@ -78,25 +103,15 @@ function create (){
             frameRate: 10,
         });
 
-// Collider
+//Collider
     this.physics.add.collider(player, platforms);
 
-// Keyboard controls
-    cursors = this.input.keyboard.createCursorKeys();
-    
+//Keyboard controls
+    cursors = this.input.keyboard.createCursorKeys(); //enables keyboard input
+  
 }
 
 function update (){
-
-//     const conditionsArray = [
-//     condition1, 
-//     condition2,
-//     condition3,
-// ]
-
-// if (conditionsArray.indexOf(false) === -1) {
-//     "do somthing"
-// }
 
 //Left  
     if (cursors.left.isDown && player.body.touching.down) {
@@ -131,7 +146,6 @@ function update (){
         player.anims.play('p1-jump', true);
     }
 
-
 //Idle   
     else {
         player.setVelocityX(0);
@@ -139,25 +153,7 @@ function update (){
     }
  
 //Attack   
-    if (
-        cursors.space.isDown
-        
-        // cursors.space.isDown && cursors.right.isDown && player.body.touching.down ||
-        // cursors.space.isDown && cursors.left.isDown ||
-        // cursors.space.isDown && cursors.left.isDown && player.body.touching.down ||
-        // cursors.space.isDown && player.body.touching.down == false
-        ) {
-        player.anims.play('p1-attack', true);
-    }
-
-    else if (
-        cursors.space.isDown
-        
-        // cursors.space.isDown && cursors.right.isDown && player.body.touching.down ||
-        // cursors.space.isDown && cursors.left.isDown ||
-        // cursors.space.isDown && cursors.left.isDown && player.body.touching.down ||
-        // cursors.space.isDown && player.body.touching.down == false
-        ) {
+    if (cursors.space.isDown) {
         player.anims.play('p1-attack', true);
     }
 
@@ -165,6 +161,17 @@ function update (){
     if (cursors.up.isDown && player.body.touching.down) {
         player.setVelocityY(-550);  
     }
+
+//Alt movement
+// char2.anims.play('p2-walk',true);
+
+char2.x-=0.5; //Moves char2 by 1px to the left
+if (char2.x < 0) { 
+    char2.x = game.config.width; //If reches left border move to right border
+    }
+
+    
+
 
     
     
