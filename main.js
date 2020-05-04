41843dcc225b2c764f479512ed17f747c78f8ca2
P <morr.conceptart@gmail.com>
2020-05-04 19:10:44 +0200

    fixed

--- main.js
+++ main.js
@@ -17,26 +17,20 @@ var config = {
                 // debug: true
             }
         },
-<<<<<<< HEAD
-<<<<<<< HEAD
         scene: [SceneMain, SceneTitle,]
-=======
-        scene: [SceneMain, SceneTitle]
->>>>>>> parent of bbb95cd... 1
-=======
-        scene: [SceneMain, SceneTitle]
->>>>>>> parent of bbb95cd... 1
     };
     game = new Phaser.Game(config);
 }
 
 var player;
 var enemy;
+
 var cursors;
 var platforms;
 var rNum;
 var distance;
 var speed;
+
 var playerHp;
 var enemyHp;
 var playerHpText;
